7c363eda26e4e10fb8ef18e360a30b64f2a7c25a
Charlie <ccariste@gmail.com>
2018-06-05 09:39:23 -1000

    CRC: get char by gender working, also add'tl linkages built in

--- characters/charactersRouter.js
+++ characters/charactersRouter.js
@@ -1,21 +1,66 @@
 const express = require('express');
 
 const Character = require('./Character.js');
+const Vehicle = require('../vehicles/Vehicle.js');
 
 const router = express.Router();
 
 router
   .route('/')
   .get((req, res) => {
-    Character.find()
-      .then( characters => {
-        console.log(characters);
-        res.status(200).json(characters);
-      })
-      .catch( err => {
-        res.status(500).json({ error: 'Error getting chars', err});
-      })
+    const { gender, height } = req.query;
+    let query = Character.find()
+    console.log('height', height);
+    if (gender !== undefined) {
+        query.where({ gender: gender })
+            .then( chars => res.json(chars))
+            .catch( err => res.status(500).json({ error: 'Error getting chars from database', err}))
+    } else {
+        query.then( chars => res.json(chars))
+    }
+});
+
+
+router
+    .route('/:id')
+    .get((req, res) => {
+      const { id } = req.params;
+      Character.findById(id)
+        .then( char => {
+          if (char !== null) {
+            res.status(200).json(char);
+          } else {
+            res.status(404).json({error: 'There are no characters with that ID.' })
+          }
+        })
+        .catch( err => {
+          res.status(500).json({error: 'Error retrieving friend from database', err});
+        })
     })
+
+// Find all vehicles driven by a given character. (/api/characters/:id/vehicles)
+
+router
+    .route('/:id/vehicles')
+    .get((req, res) => {
+      const { id } = req.params;
+      let char = Character.findById(id)
+      let query = Vehicle.find()
+      console.log('Q', query);
+      query.where({ pilots: char })
+        .then( vehicles => {
+          if (vehicles !== null) {
+            res.status(200).json(vehicles);
+          } else {
+            res.status(404).json({error: 'There are no vehicles with that character.' })
+          }
+        })
+        .catch( err => {
+          res.status(500).json({error: 'Error retrieving vehicles from database', err});
+        })
+    })
+
+// Find all female characters taller than 100cm (/api/characters?minheight=100)
 
 
 module.exports = router;
