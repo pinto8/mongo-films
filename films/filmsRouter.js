d12ad7ac38eecad53cfb28b0da47ae6c2cf8380b
Charlie <ccariste@gmail.com>
2018-06-07 16:24:42 -1000

    CRC: Film queries and sorts working, except released

--- films/filmsRouter.js
+++ films/filmsRouter.js
@@ -9,30 +9,28 @@ const router = express.Router();
 router
   .route('/')
   .get((req, res) => {
-    Film.find()
-      .then( films => {
-        console.log(Film);
-        res.status(200).json(films);
-      })
-      .catch( err => {
-        res.status(500).json({ error: 'Error getting films', err});
-      })
+    const { producer, released } = req.query;
+    let query = Film.find()
+      .sort({ episode: 1 })
+      .select('episode title planets characters producer release_date')
+      .populate('characters', `_id name gender height skin_color hair_color eye_color`)
+      .populate('planets', `name climate terrain gravity diameter`)
+      if (producer !== undefined) {
+        const producerFilter = new RegExp(producer, 'i')
+        query.where({ producer: producerFilter })
+      }
+      if (released !== undefined) {
+        query.where({ release_date: { $regex: released, $options: 'i' }})
+      } else {
+    query.then( films => {
+      console.log(Film);
+      res.status(200).json(films);
     })
-  
-  .post((req, res) => {
-    const filmData = req.body;
-    const { title } = req.body.title;
-    const film = new Film(filmData);
-    film.save()
-      .then( film => {
-        res.status(201).json(film);
-      })
-      .catch( err => {
-        res.status(500).json({error: 'Error posting to database', err});
-      })
+    query.catch( err => {
+      res.status(500).json({ error: 'Error getting films', err});
+    })
+    }
   });
-
-    
 
 // * order by episode.
 // * populate character information.
